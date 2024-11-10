84f8c8f855aee17a0b431414738a7c47e4acd83c
Eliza P <epetrycka@student.agh.edu.pl>
2024-11-10 15:12:37 +0100

    till 32 lesson

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -16,6 +19,15 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({ id: commentId, content});
 
     commentsByPostId[req.params.id] = comments;
+
+    axios.post(`http://localhost:4005/events`, {
+        type:'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
 
     res.status(201).send(comments);
 });
--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -18,6 +21,13 @@ app.post('/posts', (req, res) => {
     posts[id] = {
         id, title
     };
+
+    axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    });
 
     res.status(201).send(posts[id]);
 });
